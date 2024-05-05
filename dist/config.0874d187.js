c97abff05d82793eea4de34fdb3df607e8da3782
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-05 17:28:04 -0300

    integrar fuente local skills

--- dist/config.0874d187.js
+++ dist/config.0874d187.js
@@ -172,11 +172,11 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/loader.scss":[function(require,module,exports) {
+},{"./..\\fonts\\Robert.ttf":[["Robert.9d40c184.ttf","assets/fonts/Robert.ttf"],"assets/fonts/Robert.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/loader.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/landing.scss":[function(require,module,exports) {
+},{"./..\\fonts\\Robert.ttf":[["Robert.9d40c184.ttf","assets/fonts/Robert.ttf"],"assets/fonts/Robert.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/landing.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
@@ -184,14 +184,19 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/js/config.js":[function(require,module,exports) {
+},{"./..\\fonts\\Robert.ttf":[["Robert.9d40c184.ttf","assets/fonts/Robert.ttf"],"assets/fonts/Robert.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/skills.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./..\\fonts\\Robert.ttf":[["Robert.9d40c184.ttf","assets/fonts/Robert.ttf"],"assets/fonts/Robert.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/js/config.js":[function(require,module,exports) {
 "use strict";
 
 require("../styles/base.scss");
 require("../styles/loader.scss");
 require("../styles/landing.scss");
 require("../styles/coolLinks.scss");
-},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coolLinks.scss":"assets/styles/coolLinks.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+require("../styles/skills.scss");
+},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coolLinks.scss":"assets/styles/coolLinks.scss","../styles/skills.scss":"assets/styles/skills.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
