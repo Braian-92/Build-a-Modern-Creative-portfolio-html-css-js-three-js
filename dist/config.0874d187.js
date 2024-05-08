ad04267a15a616b1fabf6e75fa8007afc8c1d6f5
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-07 23:16:27 -0300

    inicio projects

--- dist/config.0874d187.js
+++ dist/config.0874d187.js
@@ -196,6 +196,10 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/styles/projects.scss":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
 },{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"assets/js/config.js":[function(require,module,exports) {
 "use strict";
 
@@ -206,7 +210,8 @@ require("../styles/coolLinks.scss");
 require("../styles/skills.scss");
 require("../styles/reviews.scss");
 require("../styles/swiper.scss");
-},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coolLinks.scss":"assets/styles/coolLinks.scss","../styles/skills.scss":"assets/styles/skills.scss","../styles/reviews.scss":"assets/styles/reviews.scss","../styles/swiper.scss":"assets/styles/swiper.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+require("../styles/projects.scss");
+},{"../styles/base.scss":"assets/styles/base.scss","../styles/loader.scss":"assets/styles/loader.scss","../styles/landing.scss":"assets/styles/landing.scss","../styles/coolLinks.scss":"assets/styles/coolLinks.scss","../styles/skills.scss":"assets/styles/skills.scss","../styles/reviews.scss":"assets/styles/reviews.scss","../styles/swiper.scss":"assets/styles/swiper.scss","../styles/projects.scss":"assets/styles/projects.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
