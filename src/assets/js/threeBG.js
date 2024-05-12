919c7a5efb3b6deb3e290dcd4c15bd31e8f4a45b
Braian-92 <zamudiobraianhernan@gmail.com>
2024-05-12 18:09:22 -0300

    animación tipo bandera en fondo principal con threejs

--- src/assets/js/threeBG.js
+++ src/assets/js/threeBG.js
@@ -23,11 +23,11 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-const geometry = new THREE.PlaneGeometry(5, 5, 15, 9)
+const geometry = new THREE.PlaneGeometry(18, 10, 30, 9)
 const material = new THREE.MeshBasicMaterial({
   // color: 0xff0000,
   map: loader.load(images.bg2),
-  wireframe: true
+  // wireframe: true
 })
 
 const mesh = new THREE.Mesh(geometry, material)
@@ -36,10 +36,20 @@ scene.add(mesh)
 camera.position.z = 5
 
 const count = geometry.attributes.position.count
-
+const clock = new THREE.Clock()
 
 function animate() {
+  const time = clock.getElapsedTime()
   requestAnimationFrame(animate)
+  for (let i = 0; i < count; i++) {
+    const x = geometry.attributes.position.getX(i)
+    const y = geometry.attributes.position.getY(i)
+
+    geometry.attributes.position.setZ(i, 0.9 * Math.sin( x * time * 0.3) )
+    geometry.computeVertexNormals()
+    geometry.attributes.position.needsUpdate = true
+  }
+
   // mesh.rotation.x += 0.01
   // mesh.rotation.z += 0.01
   renderer.render(scene, camera)
